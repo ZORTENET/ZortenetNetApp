ee3bccca842d9ac3d6fa05d87faf380d7c9941d5
OikonomakisAndreas <a.oikonomakis@iit.demokritos.gr>
2022-10-31 13:19:28 +0200

    added .env / new endpoint

--- rebuild_and_run.sh
+++ rebuild_and_run.sh
@@ -1,9 +1,3 @@
-#docker stop mynetapp
-#docker rm mynetapp
-#docker rmi netapp:latest
-#docker build --tag netapp .
-#docker run -d -p 5000:5000 --name mynetapp  netapp
-
 docker-compose down
-docker-compose up
-docker network connect nef_emulator_default mynetapp
+docker-compose up --build
+docker network connect nef_emulator_default zorte_netapp
